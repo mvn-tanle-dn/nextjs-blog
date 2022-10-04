d8835df81b8e0004d1f8ed9d902094e034f20d51
tan-le <tan.le@monstar-lab.com>
2022-10-04 09:12:05 +0700

    blog

---
+++ pages/posts/first-post.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import Layout from "../../components/layout";
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <h1>First Post</h1>
+      <a href="/">Back to home</a>
+    </Layout>
+  );
+}
